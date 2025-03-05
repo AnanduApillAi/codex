bf63198b0c505fd0d012d7f39f33c90c3994dbf5
Anandu A <anandua606@gmail.com>
2025-03-05 08:14:12 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import "@/app/globals.css";
+
 export const metadata: Metadata = {
   title: "CodEX",
   description: "Codex is a snippet manager that allows you to save and manage your code snippets.",
+  icons: {
+    icon: '/favicon.ico', 
+  }
 };
 
 export default function RootLayout({
@@ -12,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className="dark">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         {children}
       </body>
